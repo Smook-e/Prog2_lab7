8d75ced74eab54edd8ce679b9cc2158926c26a19
EngYoussefSharaf <sharafyoussef693@gmail.com>
2025-11-14 20:31:24 +0200

    update

--- src/Courses/Lesson.java
+++ src/Courses/Lesson.java
@@ -1,4 +1,42 @@
 package Courses;
 
 public class Lesson {
+    private int lessonId;
+    private String title;
+    private String content;
+    private String optionalResources[];
+
+    public Lesson(int lessonId, String title) {
+        this.lessonId = lessonId;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLessonId() {
+        return lessonId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String[] getOptionalResources() {
+        return optionalResources;
+    }
+
+    public void setOptionalResources(String[] optionalResources) {
+        this.optionalResources = optionalResources;
+    }
+    
+    public Lesson[] FetchLessons(Course c)
+    {
+        return c.getLessons();
+    }
 }
