ccb31460e6113eff6ef9134f23c279516b93dc9c
ritage <ritageashraf2005@gmail.com>
2025-11-14 19:04:23 +0200

    get enrolled courses and get lessons in course methods

--- src/JSON/StudentService.java
+++ src/JSON/StudentService.java
@@ -16,5 +16,31 @@ public class StudentService {
         this.courseService = courseService;
     }
 
-    
+    public List<Course> browseCourses() {
+        return courseService.getDb();  
+    }
+     public boolean enrollStudentInCourse(Student student, String courseId) {
+
+        if (student.getEnrolledCourses().contains(courseId)) {
+            return false; // already enrolled
+        }
+
+        student.getEnrolledCourses().add(courseId);
+        userService.save(); // update users.json
+        return true;
+    }
+     public List<Course> getEnrolledCourses(Student student) {
+        List<Course> result = new ArrayList<>();
+
+        for (String cid : student.getEnrolledCourses()) {
+            Course c = courseService.getCourseByID(cid);
+            if (c != null) result.add(c);
+        }
+        return result;
+    }
+     public List<Lesson> getLessonsForCourse(String courseId) {
+        Course c = courseService.getCourseByID(courseId);
+        if (c == null) return new ArrayList<>();
+        return c.getLessons();  // lessons come FROM course object
+    }
 }
