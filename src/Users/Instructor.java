2d41c7c5eaae1c07e5223a7039b1dfb06ee96648
sara-0011 <sarahabiiba@icloud.com>
2025-11-15 20:17:04 +0200

    commit to Instuctor class

--- src/Users/Instructor.java
+++ src/Users/Instructor.java
@@ -1,4 +1,17 @@
 package Users;
 
+import java.util.*;
+
 public class Instructor extends User {
+    private List<String> createdCourses;
+    public Instructor(String userID,String password,String userName,String email)
+    {
+        super( userID,  password,  userName, "Instructor", email);
+        this.createdCourses=new ArrayList<>();
+    }
+    public List<String> getCreatedCourses()
+    {
+        return createdCourses;
+    }
+    public void a
 }
