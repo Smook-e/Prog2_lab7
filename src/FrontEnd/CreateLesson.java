7f73b4f87604e47926b6d2b18975d3ed6ce33f37
sara-0011 <sarahabiiba@icloud.com>
2025-11-16 15:32:33 +0200

    commit to changes in create and edit course

--- src/FrontEnd/CreateLesson.java
+++ src/FrontEnd/CreateLesson.java
@@ -8,12 +8,12 @@ package FrontEnd;
  *
  * @author HP
  */
-public class CreatAndEditLesson extends javax.swing.JFrame {
+public class CreateLesson extends javax.swing.JFrame {
 
     /**
      * Creates new form CreatAndEditLesson
      */
-    public CreatAndEditLesson() {
+    public CreateLesson() {
         initComponents();
     }
 
@@ -113,20 +113,21 @@ public class CreatAndEditLesson extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CreatAndEditLesson.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CreateLesson.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CreatAndEditLesson.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CreateLesson.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CreatAndEditLesson.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CreateLesson.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CreatAndEditLesson.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CreateLesson.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CreatAndEditLesson().setVisible(true);
+                new CreateLesson().setVisible(true);
             }
         });
     }
